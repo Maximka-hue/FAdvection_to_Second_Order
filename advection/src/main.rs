561f765137bbf6434e939cc43fca3e93c8c7f495
Maxim <53125181+Maximka-hue@users.noreply.github.com>
2022-01-27 16:40:27 +0300

    Update main.rs

--- advection/src/main.rs
+++ advection/src/main.rs
@@ -11,7 +11,8 @@
 #![feature(path_try_exists)]
 #[macro_use]
 pub use input_structure;
-use input_structure::initial_data_utils::function_utils::print_macros::macro_lrls::{generate_random_parameters, pt};
+use input_structure::initial_data_utils::{ Path, PathBuf, function_utils::print_macros::macro_lrls::{pt}};
+use input_structure::initial_data_utils::{parse_into_file_parameters};
 use input_structure::cfutils::{ColorPrintState, ArgumentParseFilesError, op_sys};
 use input_structure::{TaskType, TaskTypeCs, FileParametres, FileParametresBuilder, initial_information_of_advection};
 #[macro_use]
@@ -24,7 +25,6 @@ extern crate os_type;
 extern crate ansi_term;
 extern crate num_cpus;
 extern crate rand;
-use itertools::Itertools;
 pub use ansi_term::{Colour::{Black as AnsiBlack, Red as AnsiRed, Green as AnsiGreen, Yellow as AnsiYellow, Blue as AnsiBlue, Purple as AnsiPurple, 
     Cyan as AnsiCyan, Fixed as AnsiFixed}, Style as AnsiStyle};
 ///These imports from library as I already downloaded these crates)
@@ -41,7 +41,12 @@ pub const PATH_DEBUG_INFO: bool = true;
 pub const PATH_CREATION: bool = true;
 pub const LETS_DO_PAUSE: bool = true;
 pub const GENERATE_RANDOM_EXAMPLE: bool = true;
+pub const RANDOM_PATH_CREATION: bool = false;
 }
+mod modifications{
+pub const RANDOM_TRANSLATE_MARGINE_BOUNDARY: bool = true;
+}
+
 fn all_colours() {
     black!("black ");
     red!("red ");
@@ -59,6 +64,7 @@ fn all_colours() {
 //))) blue for paths and debug, 
 pub use determine_my_impls::*;
 pub use determine_calculation_modes::*;
+use modifications::*;
 fn main() {
 //Here I am defining by default colored struct with task type
     let mut burger_rng = rand::thread_rng();
@@ -80,7 +86,9 @@ initial_information_of_advection();
     let animation_path = advection_path.join("animation");
     let calculation_path = animation_path.join("datas");
     let photos_path = animation_path.join("photos");
-    let input_fpath = env_path.parent().expect("Determine input file's path").join("input-structurs").join("src").join("advec_examples/");
+    let input_fpath = env_path.parent().expect("Determine input file's path").join("input-pstructures").join("src").join("advec_examples/");
+    let directory_with_examples_exists = input_fpath.try_exists().expect("Can't check existence of file does_not_exist.txt");
+    let is_example_dir: bool = if directory_with_examples_exists { input_fpath.is_dir()} else{ false};
     if PATH_DEBUG_INFO {
         assert_eq!(&env_path.as_path().display().to_string(), "/home/computadormaxim/_Programming_projects/RUSTprojects/FAdvection_to_Second_Order/advection");
         println!("{prog}  {env_path:#?}\n{anim}  {advection_path:#?}\n\
@@ -90,38 +98,40 @@ initial_information_of_advection();
         prog=  AnsiGreen.blink().paint("Programm start location:"), photo=  AnsiGreen.bold().paint("Photos path:"),
         anim= AnsiGreen.bold().paint("Animation storage"), calc=  AnsiGreen.bold().paint("Calculation data:"), 
         input_txt= AnsiGreen.bold().paint(""));
+        if !directory_with_examples_exists{
         println!("Number of threads on your Computador: {num_threads}\n\
-        You entered directory that {} to exist and {} to be directory",
-        input_fpath.try_exists().expect("Can't check existence of file does_not_exist.txt"), input_fpath.is_dir());
+        You entered path {input_fpath:?}\n that {} to exist",
+        directory_with_examples_exists);
+        }
+        else if directory_with_examples_exists && !is_example_dir{
+            println!("You entered path {input_fpath:?}\n that exists but {} to be directory\n\tPlease enter another path", is_example_dir);
+
+        } 
     }
     println!("{}", burger_order);
-    if PATH_CREATION{
+    if PATH_CREATION {
         fs::create_dir_all(&animation_path).unwrap(); 
         fs::create_dir_all(&calculation_path).unwrap(); 
         fs::create_dir_all(&photos_path).unwrap(); 
-        if !input_fpath.try_exists().expect("Can't check existence of file does_not_exist.txt") && input_fpath.is_dir(){
+        if !directory_with_examples_exists {
         fs::create_dir(&input_fpath).unwrap();
+        println!("{input_fpath:?}Is it now directory? {}" , fs::canonicalize(&input_fpath).unwrap_or(PathBuf::new()).is_dir())
         }
+    if RANDOM_PATH_CREATION {
+            if directory_with_examples_exists{
+    
+            }
+            else{
+                    //fs::create_dir().unwrap();
+            }
+    
+            }
     }
-//Then I am initializing structure that would be passed as initial datas for program
+//Then I am initializing structure that would be passed as initial datas for program, but!
     let mut dataf = FileParametres::first_initializing(3).expect("Something wrong in Initializing");
-    let (eq_type, bound_type, init_type, add_args, time_eval_period_stage, init_conditions , margin_domain, quantity_split_nodes, n_corant):
-    (i8, i8, i8, i8, (f64, f64), f64, f64, f64, f64);
-    let random_parameters = generate_random_parameters!().unwrap();
-    let int_input = random_parameters.0;
-    let float_input = random_parameters.1;
-    assert_eq!(int_input.len(), 4);
-    assert_eq!(float_input.len(), 10);
-    if let Some((req_type, rbound_type, rinit_type, radd_args)) = int_input.into_iter().tuples().next(){
-        eq_type= req_type; bound_type= rbound_type; init_type= rinit_type; add_args= radd_args;
-    };
-    if let Some((rtime_eval_period_stage_one, rtime_eval_period_stage_two, rinit_conditions , rmargin_domain, rquantity_split_nodes, rn_corant)) = float_input.into_iter().tuples().next(){
-        //eq_type= req_type; bound_type= rbound_type; init_type= rinit_type; add_args= radd_args;
-        time_eval_period_stage = (rtime_eval_period_stage_one, rtime_eval_period_stage_two);
-        println!("{:?}", time_eval_period_stage);
-    };
-    if GENERATE_RANDOM_EXAMPLE{
-
+//There are options: generate from file[GENERATE_RANDOM_EXAMPLE= false or datas from file will be illigal] and from txt files.
+if GENERATE_RANDOM_EXAMPLE{    
+    parse_into_file_parameters(RANDOM_TRANSLATE_MARGINE_BOUNDARY);
     }
     else{
         //Get txt with datas
@@ -132,6 +142,5 @@ initial_information_of_advection();
     println!("\n{}", &possible_error.to_string());
 
 }
-
 /* 
 */
