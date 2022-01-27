7a9ad5f81a74b8172cfb271b9b5c39b68d63e993
Maxim <53125181+Maximka-hue@users.noreply.github.com>
2022-01-27 13:38:06 +0300

    Update lib.rs

--- input-structures/src/lib.rs
+++ input-structures/src/lib.rs
@@ -6,6 +6,10 @@ extern crate colorify;
 #[macro_use]
 extern crate colour;
 pub mod initial_data_utils;
-pub use crate::initial_data_utils::*;
+pub use crate::initial_data_utils::function_utils::cfutils::{self, op_sys};
+pub use crate::initial_data_utils::initial_input_structures::{TaskType, TaskTypeCs, FileParametres,FileParametresBuilder, initial_information_of_advection};
+pub use crate::initial_data_utils::function_utils::print_macros::macro_lrls;
 use std::time::{Instant};
 use chrono::{Local};
+extern crate rand;
+use rand::{prelude::*, Rng, SeedableRng};
