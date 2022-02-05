eb17701d0caf3ade1b764e51048ae81bf1ee9286
Maxim <53125181+Maximka-hue@users.noreply.github.com>
2022-02-05 16:29:21 +0300

    Update smooth.rs

--- input-structures/src/smooth.rs
+++ input-structures/src/smooth.rs
@@ -1,23 +1,23 @@
-pub(crate) fn smoothZF_rs(fs: &mut Vec<f64>, Nmax: usize, smooth_int: f64, fi:  &mut Vec<f64>, ftd:  &mut Vec<f64>){
+pub fn smooth_zf_rs(fs: &mut Vec<f64>, nmax: usize, smooth_int: f64, fi:  &mut Vec<f64>, ftd:  &mut Vec<f64>){
     let mut sign: i64 = -1;
     let mut f1: f64;
     let mut f2: f64;
 
-    for n in 0.. Nmax-1{
+    for n in 0.. nmax-1{
         fi[n]= smooth_int * (fs[n+1] - fs[n]);
     }
-//So i need to determine somehow Fi[Nmax-1].
-    fi[Nmax-1] = 0.0; //Notice, please.
-    for n in 1.. Nmax-1{
+//So i need to determine somehow Fi[nmax-1].
+    fi[nmax-1] = 0.0; //Notice, please.
+    for n in 1.. nmax-1{
         ftd[n] = fs[n] + (fi[n] - fi[n-1]);
     }
     ftd[0] = fs[0];
-    ftd[Nmax -1] = fs[Nmax -1];
-    for n in 0 .. (Nmax - 1){
+    ftd[nmax -1] = fs[nmax -1];
+    for n in 0 .. (nmax - 1){
         fs[n] = ftd[n+1] - ftd[n];
     }
 //Main calculations ++++++++++++++++++++++++++++++++++++++++
-    for n in 0..Nmax-1{
+    for n in 0..nmax-1{
         fi[n] = smooth_int * (ftd[n+1] - ftd[n]);
         let sgn = fi[n].signum() as i64; // copy sign from calculated value
         if sgn >= 0 {
@@ -30,11 +30,11 @@ pub(crate) fn smoothZF_rs(fs: &mut Vec<f64>, Nmax: usize, smooth_int: f64, fi:  
                 fi[0] = f2;
             }
         }
-    else if n== Nmax-2{//Should be noticed!
-        f1 = sign as f64 * fs[Nmax - 2];
-        if  f1 < fi[Nmax - 1]//What value??? - 0;
+    else if n== nmax-2{//Should be noticed!
+        f1 = sign as f64 * fs[nmax - 2];
+        if  f1 < fi[nmax - 1]//What value??? - 0;
         {
-            fi[Nmax - 1] = f1;
+            fi[nmax - 1] = f1;
         }
     }
     else
@@ -54,7 +54,7 @@ pub(crate) fn smoothZF_rs(fs: &mut Vec<f64>, Nmax: usize, smooth_int: f64, fi:  
       {fi[n] = sign as f64 * fi[n];}
     }
 //-------------------------------------------
-    for n in 1..Nmax{
+    for n in 1..nmax{
         fs[n] = ftd[n] - (fi[n] - fi[n-1]);//fi[last] = 0;
         fs[0] = ftd[0];
     }
